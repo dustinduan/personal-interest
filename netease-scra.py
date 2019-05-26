2883a7a230f02d68a5fef19f675818c1fba81f0d
dustinduan2010 <dustinduan2010@163.com>
2019-05-26 11:07:37 +0800

    网易学习数据分析的第一个简单爬虫，爬取豆瓣的图书信息

---
+++ netease-scra.py
@@ -0,0 +1,22 @@
+import requests as req
+from bs4 import BeautifulSoup as bup
+import pandas as pd
+basic_web='https://book.douban.com/latest'
+u=req.get(url=basic_web)
+#print(u)验证是否访问成功
+soup=bup(u.text,'lxml')
+infor=soup.find_all('div',class_="detail-frame")
+lis=[]
+for i in infor:
+    dic={}
+    dic['书名']=i.h2.text.replace("\n","")
+    dic['评分']=i.find_all('p')[0].text.replace("\n","").replace(" ","")
+    dic['其他信息']=i.find_all('p')[1].text.replace("\n","").replace(" ","")
+    dic['简介']=i.find_all('p')[2].text.replace("\n","").replace(" ","")
+    lis.append(dic)
+    
+
+
+
+df=pd.DataFrame(lis)
+df
