7921da3e632e1214914a9d4525f2277a78a7d9ff
dustinduan <dustinduan2010@163.com>
2020-04-04 18:59:49 +0800

    jupyter info change

--- ip_locate.py
+++ ip_locate.py
@@ -1,24 +1,27 @@
 #appcode:91fa754784494761b450672aba2d3efb
 #appkey:anbbjlcmg3dfu6si1uvlo61xt3xy27ti
-import urllib, urllib3, sys
+import urllib.request as ul
 import ssl
+import sys
 #python3示例代码下载链接
 #http://code.fegine.com/python3Demo.zip
 
+ip=input('请输入需要定位的Ip地址:')
 host = 'https://ips.market.alicloudapi.com'
 path = '/iplocaltion'
 method = 'GET'
 appcode = 'anbbjlcmg3dfu6si1uvlo61xt3xy27ti'
-querys = 'ip=127.0.0.1'
+querys = 'ip='+ip
 bodys = {}
 url = host + path + '?' + querys
 
-request = urllib.Request(url)
-request.add_header('Authorization', 'APPCODE ' + appcode)
+head={'Authorization':'APPCODE '+appcode}
+request = ul.Request(url,headers=head)
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
-response = urllib.urlopen(request, context=ctx)
-content = response.read()
-if (content):
-    print(content)
+response=ul.urlopen(request,context=ctx)
+#content=response.read()
+#if (content):
+#    print(content)
+print(response)
